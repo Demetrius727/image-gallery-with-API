0ecbd549c114d86369adfcc934205dd801cfd5aa
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2024-12-05 10:43:40 -0300

    Implantando e testando primeiro contato com API unsplash

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,27 @@ import Searchbar from './components/Searchbar'
 
 import { useState, useEffect } from 'react';
 
-function App() {
+import axios from "axios";
 
+function App() {  
+  const [query, setQuery] = useState("");
+  const [categoria, setCategoria] = useState("");
+
+  const fetchData = async ({ query, categoria }) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        client_id: apiKey,
+      }
+    });
+
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchData(query, categoria);
+  }, [])
 
   return (
     <div className='container'>
