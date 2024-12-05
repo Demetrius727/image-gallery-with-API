157ce93167b8421df56f16f40517bbbef391b0ee
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2024-12-05 12:51:15 -0300

    Criando componente Seachbar e estilizando a página no index.css

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const Searchbar = () => {
+
+    const categorias = [
+        "Natureza",
+        "Pessoas",
+        "Tecnologia",
+        "Animais",
+        "Esportes"
+    ]
+
   return (
-    <div>Searchbar</div>
+    <div className='search-bar'>
+        <input type='text' placeholder='   Pesquisar fotos...'/>
+        <button>Pesquisar</button>
+        <select>
+            {categorias.map((categoria) => (
+                <option key={categoria} value={categoria}>{categoria}</option> // Categoria é unica então usou também como key
+            ))}
+        </select>
+    </div>
   )
 }
 
