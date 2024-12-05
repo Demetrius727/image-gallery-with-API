39b1112f72ee625a304ea88206f80d809c05d1b3
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2024-12-05 14:39:34 -0300

    Adicionando try/catch ao axios

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 
-const Searchbar = ({setQuery, setCategoria, setActivateSearch}) => {
+const Searchbar = ({ setQuery, setCategoria, setActivateSearch }) => {
+    const [localQuery, setLocalQuery] = useState(""); // Campo de entrada é controlado localmente
 
     const categorias = [
         "Natureza",
         "Pessoas",
         "Tecnologia",
         "Animais",
-        "Esportes"
+        "Esportes",
+        "Arte"
     ]
 
     return (
         <div className='search-bar'>
-            <input type='text' placeholder='   Pesquisar fotos...' onChange={(e) => setQuery(e.target.value)} />
-            <button onClick={() => setActivateSearch(true)}>Pesquisar</button>
+            <input type='text' value={localQuery} placeholder='   Pesquisar fotos...' onChange={(e) => setLocalQuery(e.target.value)} />
+            <button onClick={() => {
+                setQuery(localQuery);
+                setActivateSearch(true);
+            }}>Pesquisar</button>
             <select onChange={(e) => {
                 setCategoria(e.target.value);
                 setActivateSearch(true);
             }}>
+                <option value="">Todas as categorias</option>
                 {categorias.map((categoria) => (
                     <option key={categoria} value={categoria}>{categoria}</option> // Categoria é unica então usou também como key
                 ))}
